7dae5c9c0922439f7e1a395bc0d29dd15f11cfe1
Irfan Tito Kurniawan <42381907+titoirfan@users.noreply.github.com>
2021-02-28 16:35:44 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -1,22 +1,55 @@
-/** EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
-*   Modul               : 
-*   Soal                : 2
-*   Hari dan Tanggal    : 
-*   Nama (NIM)          : 
-*   Asisten (NIM)       : 
-*   Nama File           : soal-02.c
-*   Deskripsi           : Deskripsi file ini.
+/** EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+*   Modul               : 3
+*   Nama (NIM)          : Danu Ihza Pamungkas (13218022)
+*   Nama File           : M3.c
+*   Deskripsi           : Program search engine sederhana.
 */
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <string.h> 
 
-int main() {
-    // Tulis kode anda di sini
+void bubble_sort(char list[], int n){
+    int c, d, t;
+
+    for (c = 0 ; c < n - 1; c++) {
+        for (d = 0 ; d < n - c - 1; d++) {
+            if (list[d] > list[d+1]) {
+                /* Swapping */
+                t         = list[d];
+                list[d]   = list[d+1];
+                list[d+1] = t;
+            }
+        }
+    }
+}
+
+int main(){                               //ganti spasi dengan underline
+    char cowo[20], cewe[20];           //panjang maksimal 20 huruf 
+
+    // char *cowo, *cewe;
+    int count=0;                           
+    printf("Masukkan nama cowo: ");
+    scanf("%s", &cowo);
+    printf("Masukkan nama cewe: ");
+    scanf("%s", &cewe);
+
+    bubble_sort(cowo, strlen(cowo));
+    bubble_sort(cewe, strlen(cewe));
     
-    // Template print untuk mempermudah autograding
-    // printf("Kecocokan: %d Persen", kecocokan);
-    // printf("Maaf Anda berdua tidak cocok.");
-    return 0;
+    int shortest=strlen(cowo);
+    if(shortest>strlen(cewe)){
+        shortest=strlen(cewe);
+    }
+    
+    for(int i=0; i<shortest; i++){
+        if(cowo[i]==cewe[i]){
+            count++;
+        }
+    }
+
+    if(count>0){
+        printf("\nKecocokan: %d Persen", count*100/shortest);
+    } else{
+        printf("\nMaaf Anda berdua tidak cocok.");
+    }
 }
